da92ce68fb386daefd888a5ecdddcd23a29b476e
Matheus Battisti <matheus.battisti@essentia.com.br>
2021-10-18 15:53:14 -0300

    add javascript

---
+++ scripts.js
@@ -0,0 +1,32 @@
+console.log("Olá!");
+
+// console
+console.error("Este é um erro!");
+console.warn("Este é um aviso!");
+console.log("Esta é uma mensagem!");
+
+// variáveis
+var x = 10;
+let y = 15;
+const z = 20;
+
+console.log(x);
+console.log(y);
+console.log(z);
+
+// tipos de dados
+const name = "Matheus";
+const shirtsQty = 4;
+const isApproved = false;
+let surname = null;
+let age;
+const languages = ["JavaScript", "PHP", "Python"];
+const user = { email: "matheus@teste.com", password: "teste123" };
+
+console.log(name);
+console.log(shirtsQty);
+console.log(languages);
+
+console.log(typeof name); // String
+console.log(typeof surname); // Null
+console.log(typeof user); // Object
